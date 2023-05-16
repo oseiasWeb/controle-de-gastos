41dcb0dad6741ae34127872c98e79a1a189a8f4b
Oseias Nogueira <oseias.n.m@gmail.com>
2023-05-16 11:42:59 -0300

    Validação de mensagens para campos obrigatórios do login

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();    
+}
 
-    const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
 
 }
 
@@ -13,6 +15,40 @@ function isEmailValid() {
         return false;
     }
     return validateEmail(email);
+}
+
+function toggleEmailErrors() {
+    const email = document.getElementById('email').value;
+
+    if(!email) {
+        document.getElementById('email-required-error').style.display = "block";
+    } else {
+        document.getElementById('email-required-error').style.display = "none";
+    }
+
+    if(validateEmail(email)) {
+        document.getElementById('email-invalid-error').style.display = "none";
+    } else {
+        document.getElementById('email-invalid-error').style.display = "block";
+    }
+}
+
+function togglePasswordErrors() {
+    const password = document.getElementById('password').value;
+
+    if(!password){
+        document.getElementById('password-required-error').style.display = "block";
+    } else {
+        document.getElementById('password-required-error').style.display = "none";
+    }
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
 }
 
 function isPasswordValid() {
