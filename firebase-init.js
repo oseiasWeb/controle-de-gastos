a2e471e2f1303365bef1c0302ed462f22b066612
Oseias Nogueira <oseias.n.m@gmail.com>
2023-05-19 00:19:23 -0300

    Criação de registro de novo usuário

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpIktFK0g3dDIcH7rUl_HRHlh9UBmmwhM",
+    authDomain: "controle-de-gastos-92dc0.firebaseapp.com",
+    projectId: "controle-de-gastos-92dc0",
+    storageBucket: "controle-de-gastos-92dc0.appspot.com",
+    messagingSenderId: "1083730480279",
+    appId: "1:1083730480279:web:973089f6902f11e35d6dd2"
+};
+firebase.initializeApp(firebaseConfig);
