e24b809b69fae88bc83207ae2333986ec0d9776c
Oseias Nogueira <oseias.n.m@gmail.com>
2023-06-01 13:04:52 -0300

    Simulando os dados do Backend via JS

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,3 +5,79 @@ function logout() {
         alert('Erro ao fazer logout');
     })
 }
+
+findTransaction();
+
+function findTransaction() {
+    setTimeout(() => {
+        addTransactionsToScreen(fakeTransactions);
+    }, 1000)
+}
+
+function addTransactionsToScreen(transactions) {
+    const orderedList = document.getElementById('transactions');
+
+    transactions.forEach(transaction => {
+        const li = document.createElement('li');
+        li.classList.add(transaction.type);
+
+        const date = document.createElement('p');
+        date.innerHTML = formatDate(transaction.date);
+        li.appendChild(date);
+
+        const money = document.createElement('p');
+        money.innerHTML = formatMoney(transaction.money);
+        li.appendChild(money);
+
+        const type = document.createElement('p');
+        type.innerHTML = transaction.transactionType;
+        li.appendChild(type);
+
+        orderedList.appendChild(li);
+    });
+
+}
+
+function formatDate(date) {
+    return new Date(date).toLocaleDateString('en-us');
+}
+
+function formatMoney(money) {
+    return `${money.currency} ${money.value.toFixed(2)}` 
+}
+
+const fakeTransactions = [{
+    type: 'expense',
+    date: '2023-05-01',
+    money: {
+        currency: 'EUR',
+        value: 350
+    },
+    transactionType: 'Comida'
+}, {
+    type: 'expense',
+    date: '2023-05-01',
+    money: {
+        currency: 'EUR',
+        value: 95
+    },
+    transactionType: 'Internet'
+}, {
+    type: 'income',
+    date: '2023-05-01',
+    money: {
+        currency: 'EUR',
+        value: 2500
+    },
+    transactionType: 'Salário',
+    description: 'Empresa ABC'
+},{
+    type: 'expense',
+    date: '2023-05-01',
+    money: {
+        currency: 'EUR',
+        value: 650
+    },
+    transactionType: 'Aluguel',
+    description: 'Tipo - T3'
+}]
