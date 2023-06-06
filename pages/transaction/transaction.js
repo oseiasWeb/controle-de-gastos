290234b342d089c42b146b208a9e15f2d4d37092
Oseias Nogueira <oseias.n.m@gmail.com>
2023-06-06 20:58:00 -0300

    Salvando transações no BD do Firebase via JS

--- pages/transaction/transaction.js
+++ pages/transaction/transaction.js
@@ -1,3 +1,37 @@
+function saveTransaction() {
+    showLoading();
+
+    const transaction = createTransaction();
+
+        firebase.firestore()
+            .collection('transactions')
+            .add(transaction)
+            .then(() => {
+                hideLoading();
+                window.location.href = "../home/home.html";
+            })
+            .catch(() => {
+                hideLoading();
+                alert('Erro ao salvar transação!');
+            })
+}
+
+function createTransaction() {
+    return {
+        type: form.typeExpense().checked ? "expense" : "income",
+        date: form.date().value,
+        money: {
+            currency: form.currency().value,
+            value: parseFloat(form.value().value)
+        },
+        transactionType: form.transactionType().value,
+        description: form.description().value,
+        user: {
+            uid: firebase.auth().currentUser.uid
+        }
+    }
+}
+
 function onChangeDate() {
     const date = form.date().value;
     form.dateRequiredError().style.display = !date ? "block" : "none";
@@ -5,16 +39,16 @@ function onChangeDate() {
     toggleSaveButtonDisable();
 }
 
-function onChangeValue(){
-   const value = form.value().value;
-   form.valueRequiredError().style.display = !value ? "block" : "none";  
+function onChangeValue() {
+    const value = form.value().value;
+    form.valueRequiredError().style.display = !value ? "block" : "none";
 
-   form.valueLessOrEqualsToZeroError().style.display = value <= 0 ? "block" : "none";
+    form.valueLessOrEqualsToZeroError().style.display = value <= 0 ? "block" : "none";
 
-   toggleSaveButtonDisable();
+    toggleSaveButtonDisable();
 }
 
-function onChangeTransactionType(){
+function onChangeTransactionType() {
     const transactionType = form.transactionType().value;
     console.log(transactionType);
     form.transactionTypeRequiredError().style.display = !transactionType ? "block" : "none";
@@ -28,17 +62,17 @@ function toggleSaveButtonDisable() {
 
 function isFormValid() {
     const date = form.date().value;
-    if(!date){
+    if (!date) {
         return false;
     }
 
     const value = form.value().value;
-    if(!value || value <= 0){
+    if (!value || value <= 0) {
         return false;
     }
 
     const transactionType = form.transactionType().value;
-    if(!transactionType){
+    if (!transactionType) {
         return false;
     }
 
@@ -47,13 +81,19 @@ function isFormValid() {
 }
 
 const form = {
+    currency: () => document.getElementById('currency'),
+
     date: () => document.getElementById('date'),
     dateRequiredError: () => document.getElementById('date-required-error'),
+
+    description: () => document.getElementById('description'),
 
     saveButton: () => document.getElementById('save-button'),
 
     transactionType: () => document.getElementById('transaction-type'),
     transactionTypeRequiredError: () => document.getElementById('transaction-type-required-error'),
+
+    typeExpense: () => document.getElementById('expense'),
 
     value: () => document.getElementById('value'),
     valueRequiredError: () => document.getElementById('value-required-error'),
